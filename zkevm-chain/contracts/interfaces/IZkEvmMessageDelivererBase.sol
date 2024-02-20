4ba43e20c5ddf8df8036e1b4168752ed80516960
kylegranger <kyle.robert.granger@gmail.com>
2024-02-20 14:50:22 +0100

    remove unused chain files

--- zkevm-chain/contracts/interfaces/IZkEvmMessageDelivererBase.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity <0.9.0;
-
-interface IZkEvmMessageDelivererBase {
-  /// @notice Returns the address of the caller that dispatched the message.
-  function messageOrigin () external view returns (address);
-}
--- zkevm-chain/coordinator/src/lib.rs
+++
@@ -1,6 +0,0 @@
-pub mod config;
-pub mod faucet;
-pub mod macros;
-pub mod shared_state;
-pub mod structs;
-pub mod utils;
